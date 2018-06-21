e9453023b5c5195ee4d4fe3b995149d3f637ed44
DEdouard22 <dedouard@live.com>
2018-06-21 18:09:55 -0400

    Revert "Revert "used spread function to complete exercise 2 and 3""
    This reverts commit be7a891eeaa9bab7e65af19abb3711ae31250393.

--- public/exercise2/reducer.js
+++ public/exercise2/reducer.js
@@ -6,12 +6,23 @@ let initialState = {
 //     state object
 const reducer = (state = initialState, action) => {
 	let { type, counterIndex } = action;
+	let stateCopy = {
+		...state,
+		counters: [
+			...state.counters
+		]
+	};
 	if (type == "INCREMENT") {
-		state.counters[counterIndex]++;
-		return state;
+		//state.counters[counterIndex]++;
+		//return state;
+		stateCopy.counters[counterIndex]++;
+		return stateCopy;
 	} else if (type == "ADD_COUNTER") {
-		state.counters.push(0);
-		return state;
+		//state.counters.push(0);
+		//return state;
+		stateCopy.counters.push(0);
+		return stateCopy;
+
 	} else {
 		return state;
 	}
